929f8128ba8aa1f5bfa85c9fba729e4e2fbc3354
Nathan Mkaya <nathanmkaya@gmail.com>
2019-11-30 12:35:36 +0300

    Initial commit

---
+++ app/src/main/java/dev/nathanmkaya/showcasejava/Showcase.java
@@ -0,0 +1,11 @@
+package dev.nathanmkaya.showcasejava;
+
+import android.app.Application;
+
+public class Showcase extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
